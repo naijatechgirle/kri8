b5529cf1070f810a5f4ba1dd7ae208565857e2a3
naijatechgirle <kellyeluagu@gmail.com>
2023-10-11 21:05:58 +0100

    Refactored code and implemented countdown termination at zero.

--- dist/index.js
+++ dist/index.js
@@ -8,61 +8,62 @@ let showAlertDiv = document.getElementById('showDiv');
 let alertContent = document.getElementById('alertContent');
 
 let countDown = () => {
-    //get currentDate date
-    let newDate = new Date('November 8 2025 00:00:00').getTime();
-    //get current date
-    let currentDate = new Date().getTime();
-    // get the differnce in date
-    let yearGap = newDate - currentDate;
-    
-    let dayUpdate = Math.floor(yearGap / 1000 / 60 / 60 / 24);
-    let hrUpdate = Math.floor(yearGap / 1000 / 60 / 60) % 24;
-    let minUpdate = Math.floor(yearGap / 1000 / 60 ) % 60;
-    let secUpdate = Math.floor(yearGap / 1000 ) % 60;
-    // insert into html 
-    dayCount.innerText = dayUpdate < 10 ? '0' + dayUpdate : dayUpdate;   
-    hrCount.innerText = hrUpdate < 10 ? '0' + hrUpdate : hrUpdate;
-    minCount.innerText = minUpdate < 10 ? '0' + minUpdate : minUpdate;
-    minCount.innerText = minUpdate < 10 ? '0' + minUpdate : minUpdate;
-    secCount.innerText = secUpdate < 10 ? '0' + secUpdate : secUpdate; 
+ //get currentDate date
+ let newDate = new Date('November 1 2023 00:00:00').getTime();
+ //get current date
+ let currentDate = new Date().getTime();
+ // get the differnce in date
+ let yearGap = newDate - currentDate;
 
-    if(dayCount.innerText == 00 && hrCount.innerText == 00 && minCount.innerText == 00 && secCount == 00) {
-        // clear setInterval after newDate is reached
-        clearInterval(countDownInterval);
-    }
+ //If the difference between the future date and the current date reaches zero, terminate the countdown by clearing the interval.
+ if (yearGap <= 0) {
+  clearInterval(countDownInterval);
+  [dayCount, hrCount, minCount, secCount].forEach((el) => (el.innerText = '0'));
+  return;
+ }
+
+ let dayUpdate = Math.floor(yearGap / 1000 / 60 / 60 / 24);
+ let hrUpdate = Math.floor(yearGap / 1000 / 60 / 60) % 24;
+ let minUpdate = Math.floor(yearGap / 1000 / 60) % 60;
+ let secUpdate = Math.floor(yearGap / 1000) % 60;
+ // insert into html
+ dayCount.innerText = dayUpdate < 10 ? '0' + dayUpdate : dayUpdate;
+ hrCount.innerText = hrUpdate < 10 ? '0' + hrUpdate : hrUpdate;
+ minCount.innerText = minUpdate < 10 ? '0' + minUpdate : minUpdate;
+ secCount.innerText = secUpdate < 10 ? '0' + secUpdate : secUpdate;
 };
 // set timer to every 1000s
 let countDownInterval = setInterval(countDown, 1000);
 // email authentication
 let checkEmail = () => {
-    //email regex
-    let emailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    //loads html page fxn
-    let autoRefresh =()=>{
-        window.location.reload();
-    }
+ //email regex
+ let emailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+ //loads html page fxn
+ let autoRefresh = () => {
+  window.location.reload();
+ };
 
-    if(email.value.match(emailCheck)){
-        main.style.display = 'none';
-        let fadeDiv=()=> {
-            // displays alertDiv
-            showAlertDiv.style.display = 'block';
-        }
-        setTimeout(fadeDiv, 1000);
-        //delay after alertDiv
-        setTimeout(autoRefresh, 2000); 
-    } else {
-        let alertDiv =()=>{
-            main.style.display = 'none';
-            showAlertDiv.style.display = 'block';
-            // displays alertDiv and changes innerText
-            alertContent.innerText = 'Wrong email format 😐😐';
-            alertContent.style.padding = '10px 20px';
-        }
-        setTimeout(alertDiv, 1000);
-        //delay after alertDiv
-        setTimeout(autoRefresh, 2000); 
-    }
-}
+ if (email.value.match(emailCheck)) {
+  main.style.display = 'none';
+  let fadeDiv = () => {
+   // displays alertDiv
+   showAlertDiv.style.display = 'block';
+  };
+  setTimeout(fadeDiv, 1000);
+  //delay after alertDiv
+  setTimeout(autoRefresh, 2000);
+ } else {
+  let alertDiv = () => {
+   main.style.display = 'none';
+   showAlertDiv.style.display = 'block';
+   // displays alertDiv and changes innerText
+   alertContent.innerText = 'Wrong email format 😐😐';
+   alertContent.style.padding = '10px 20px';
+  };
+  setTimeout(alertDiv, 1000);
+  //delay after alertDiv
+  setTimeout(autoRefresh, 2000);
+ }
+};
 //added event listener
 btn.addEventListener('click', checkEmail);
